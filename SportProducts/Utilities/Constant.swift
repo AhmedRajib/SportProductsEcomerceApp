9f0467a1fcb14f403cca39c8ac630e241a0eb3a4
AhmedRajib <ahmedrajibbb@gmail.com>
2022-09-22 02:00:13 +0600

    ProjectSetup And FooterView

---
+++ SportProducts/Utilities/Constant.swift
@@ -0,0 +1,29 @@
+//
+//  Constant.swift
+//  SportProducts
+//
+//  Created by MacBook Pro on 22/9/22.
+//
+
+import SwiftUI
+
+
+
+// DATA
+// COLOR
+    
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+
+// LAYOUT
+// UX
+// API
+// IMAGE
+// FONT
+// STRING
+// MISC
+
+
+// MARK:-
+
